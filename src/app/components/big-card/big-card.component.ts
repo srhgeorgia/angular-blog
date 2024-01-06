61c20bddf3c168b3db5571752e8148825d258433
srhgeorgia <sgeorgia0297@gmail.com>
2024-01-06 02:04:57 -0300

    change components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
   imports: [],
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.css'
+  styleUrl: './big-card.component.css',
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
 
+  @Input()
+  photo: string = '';
+  @Input()
+  title: string = '';
+  @Input()
+  description: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void{}
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+
+  @Input()
+  photo: string = '';
+  @Input()
+  title: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
